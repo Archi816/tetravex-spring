d7b35964ec7358a54fec7f36d351e75124d029a1
Archi816 <gaonly16@gmail.com>
2025-03-02 18:18:36 +0100

    week 3(tests & GameManager.java, Grid.java, Tile.java

---
+++ sk/tuke/kpi/kp/src/tetravex/GameManager.java
@@ -0,0 +1,33 @@
+package sk.tuke.kpi.kp.src.tetravex;
+
+import java.util.Random;
+
+public class GameManager {
+    private Grid grid;
+    private Tile[] tiles;
+
+    public GameManager(int size) {
+        this.grid = new Grid(size);
+        this.tiles = new Tile[size * size];
+        generateTiles();
+    }
+
+    private void generateTiles() {
+        Random rand = new Random();
+        for (int i = 0; i < tiles.length; i++) {
+            tiles[i] = new Tile(rand.nextInt(10), rand.nextInt(10), rand.nextInt(10), rand.nextInt(10));
+        }
+    }
+
+    public boolean placeTile(Tile tile, int x, int y) {
+        if (grid.isPlacementValid(tile, x, y)) {
+            return grid.placeTile(tile, x, y);
+        }
+        return false;
+    }
+
+    public boolean checkWinCondition() {
+        return grid.isGameWon();
+    }
+}
+
---
+++ sk/tuke/kpi/kp/src/tetravex/Tile.java
@@ -0,0 +1,24 @@
+package sk.tuke.kpi.kp.src.tetravex;
+
+public class Tile {
+    private int top;
+    private int right;
+    private int bottom;
+    private int left;
+    private boolean placed;
+
+    public Tile(int top, int right, int bottom, int left) {
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+        this.left = left;
+        this.placed = false;
+    }
+
+    public int getTop() { return top; }
+    public int getRight() { return right; }
+    public int getBottom() { return bottom; }
+    public int getLeft() { return left; }
+    public boolean isPlaced() { return placed; }
+    public void setPlaced(boolean placed) { this.placed = placed; }
+}
